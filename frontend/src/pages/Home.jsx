29330ee319764131eda48ea649321e2f0dd70d4c
filip-postolov <postolovfilip@gmail.com>
2026-03-08 22:18:15 +0100

    add button to view projects

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,8 +1,14 @@
+import { Link } from "react-router-dom";
+
 export default function Home() {
   return (
     <div className="container">
       <h1>Filip Postolov - DevOps Portfolio</h1>
       <p>AWS | Kubernetes | Docker | CI/CD</p>
+
+      <Link to="/projects">
+        <button>View Projects</button>
+      </Link>
     </div>
   );
 }
